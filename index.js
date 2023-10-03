32e20c4c579e7a9f91d4a88fac63d5f50f4a03c8
Godfrey Karugu <karugugk@gmail.com>
2023-10-03 12:44:08 +0300

    completed assignment on control flow

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  // let ride;
+  let greetings;
+  if (ride <= 400){
+    greetings ='This one is on me!';
+  }else if(ride > 2000){
+    greetings = 'I will gladly take your thirty bucks.';
+  }else if(ride >2500){
+    greetings = 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+
+  city ==='NYC'? "Ok, sounds good.": "No go."
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch('tip'){
+    case 'genereous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you. ";
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
